dbb63a4ea6e13ba9bcf9b2a95d8ec1550a970abc
Mad_Vitaliy <omelchenko.vitaliy1@gmail.com>
2021-04-29 20:28:03 +0300

    First commit. Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Item } from './item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Lec-Ex';
+export class AppComponent implements OnInit {
+
+  isSpecial = true;
+  isActive = true;
+  
+  nullCustomer = null;
+  currentCustomer = {
+    name: 'Laura'
+  };
+
+  item: Item; // defined to demonstrate template context precedence
+  items: Item[];
+
+  currentItem: Item;
+
+  itemsNoTrackByCount   = 0;
+  itemsWithTrackByCount = 0;
+  itemIdIncrement = 1;
+
+  ngOnInit() {
+    this.resetItems();
+  }
+
+  isActiveToggle() {
+    this.isActive = !this.isActive;
+  }
+
+  giveNullCustomerValue() {
+    this.nullCustomer = 'Kelly';
+  }
+
+  resetItems() {
+    this.items = Item.items.map(item => item.clone());
+    this.currentItem = this.items[0];
+    this.item = this.currentItem;
+  }
+
+  resetList() {
+    this.resetItems();
+    this.itemsWithTrackByCount = 0;
+    this.itemsNoTrackByCount = 0;
+  }
+
+  changeIds() {
+
+    this.items.forEach(i => i.id += 1 * this.itemIdIncrement);
+    this.itemsNoTrackByCount = ++this.itemsNoTrackByCount;
+    this.itemsWithTrackByCount = ++this.itemsWithTrackByCount;
+  }
+
+  trackByItems(index: number, item: Item): number { return item.id; }
+  trackById(index: number, item: any): number { return item.id; }
 }
---
+++ src/app/item-detail/item-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+import { Item } from '../item';
+
+@Component({
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.component.html'
+})
+export class ItemDetailComponent {
+
+  @Input() item: Item;
+  constructor() { }
+}
